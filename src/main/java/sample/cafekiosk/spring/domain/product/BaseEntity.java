3ad5029eca05e0229d2025739a3c9f6feabc86ae
wlshooo <kswlsgh@naver.com>
2024-08-23 22:36:08 +0900

    Persistence Layer test(1)

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/BaseEntity.java
@@ -0,0 +1,22 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+}
---
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductSellingStatus.java
@@ -0,0 +1,20 @@
+package sample.cafekiosk.spring.domain.product;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public enum ProductSellingStatus {
+    SELLING("판매 중"),
+    HOLD("판매 보류"),
+    STOP_SELLING("판매 중지");
+
+    private final String text;
+
+    public static List<ProductSellingStatus> forDisPlay() {
+        return List.of(SELLING, HOLD);
+    }
+}
